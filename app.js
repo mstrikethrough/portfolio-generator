e16e3954ac46fa8cf7f675867d075aa55c6f580b
mstrikethrough <avila_024@yahoo.com>
2021-06-02 14:26:01 -0500

    modularized code

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
+const [userName, github] = profileDataArgs;
 
-    // this...
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-    console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//
+//    // this...
+//    for (let i = 0; i < profileDataArr.length; i+= 1) {
+//    console.log(profileDataArr[i]);
+//    }
+//
+//    console.log('================');
+//
+//    // is the same as this...
+//    profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
+//
+//printProfileData(profileDataArgs);
 
-    console.log('================');
+fs.writeFile('./index.html', generatePage(userName, github), err => {
+    if (err) throw new Error(err);
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
